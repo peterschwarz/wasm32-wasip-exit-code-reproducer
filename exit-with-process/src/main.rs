addb8dbe4cc8ca4065549929baa5d7d22968c896
Peter Schwarz <pschwarz@bitwise.io>
2024-09-17 12:11:17 -0500

    Add no_mangle to main
    This change adds no_mangle to the main function, in order to make it
    easier to find when using tools such as wasm2wat.

--- exit-with-process/src/main.rs
+++ exit-with-process/src/main.rs
@@ -1,5 +1,6 @@
 /// This example main uses [`std::process::exit`], which works as expected when compiled against the
 /// `wasm32-wasip1` target.
+#[no_mangle]
 fn main() {
     println!("Exiting with code 10");
     std::process::exit(10);
